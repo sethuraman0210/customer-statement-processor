123f092e2f248835afdd1e5ac090e420581df7da
sethuramans <sethuraman.rit@gmail.com>
2020-08-10 11:01:08 +0530

    Updated comments

--- src/main/java/com/rabobank/customer/exception/StatementValidationException.java
+++ src/main/java/com/rabobank/customer/exception/StatementValidationException.java
@@ -1,5 +1,13 @@
 package com.rabobank.customer.exception;
 
+/**
+ * StatementValidationException for the custom validation exception handling for the statement processor.
+ *
+ * @author Sethuraman Sathiyamoorthy
+ * @version 1.0
+ * @since 1.0
+ */
+
 public class StatementValidationException extends Exception{
    public StatementValidationException(){
         super();
